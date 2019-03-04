70404b16976e0368f9f32392c6eec5b3e9c5bfe4
Matthew Turnshek <matthewturnshek@gmail.com>
2019-03-04 17:38:25 -0500

    -)

--- src/bridge.cpp
+++ src/bridge.cpp
@@ -104,10 +104,10 @@ create_bidirectional_prefix_bridge(
   BridgeHandles handles;
   handles.bridge1to2 = create_bridge_from_1_to_2(
     ros1_node, ros2_node,
-    ros1_type_name, topic_name, queue_size, ros2_type_name, prefix + topic_name), queue_size);
+    ros1_type_name, topic_name, queue_size, ros2_type_name, prefix + topic_name, queue_size);
   handles.bridge2to1 = create_bridge_from_2_to_1(
     ros2_node, ros1_node,
-    ros2_type_name, prefix + topic_name), queue_size, ros1_type_name, topic_name, queue_size,
+    ros2_type_name, prefix + topic_name, queue_size, ros1_type_name, topic_name, queue_size,
     handles.bridge1to2.ros2_publisher);
   return handles;
 }
